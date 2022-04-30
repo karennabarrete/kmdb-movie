4119bd6541aa86b650bb4222dc66721830492266
karennabarrete <karen.nabarrete.garcia@gmail.com>
2022-04-30 18:05:11 -0300

    Ajuste e Conexão do Banco de Dados

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 
+const db = require("./src/database");
+
 const app = express();
 
 app.get("/", (req,res) => {
     res.send ("Hi!");
 });
+
+db.hasConection ();
 
 app.listen (3000, () => console.log ("Servidor rodando na porta 3000"));
